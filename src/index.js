a5864c9c9a39cffd27d4d2db1d67926bf0b85b79
Feud-Rose <145285546+Feud-Rose@users.noreply.github.com>
2023-12-15 15:48:18 -0700

    Add "Menu" tab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-console.log("hotdog")
 import './style.css'
 import loadHome from './pages/Home'
 import loadAbout from './pages/About'
+import loadMenu from './pages/Menu'
 
 (function loadSite() {
 
@@ -38,7 +38,7 @@ import loadAbout from './pages/About'
     navigation.addEventListener('click', (e) => {
             
             let target = e.target.classList
-            console.log(target)
+           
             
 
       /* Create separate module too handle this */
@@ -46,16 +46,48 @@ import loadAbout from './pages/About'
                 content.textContent= ""
                 aboutSelect.classList.remove("inactive")
                 aboutSelect.classList.add("active")
-                if(homeSelect.classList[1] == "active"){
+                if (homeSelect.classList[1] == "active"){
                     homeSelect.classList.remove("active")
                     homeSelect.classList.add("inactive")
                 }
-
+                else if (aboutSelect.classList[1] == "active"){
+                    menuSelect.classList.remove("active")
+                    menuSelect.classList.add("inactive")
+                }
                 loadAbout()
+            }
+            else if(target[0] == "menuBtn"){
+                content.textContent= ""
+                menuSelect.classList.remove("inactive")
+                menuSelect.classList.add("active")
+                if (homeSelect.classList[1] == "active"){
+                    homeSelect.classList.remove("active")
+                    homeSelect.classList.add("inactive")
+                }
+                else if (aboutSelect.classList[1] == "active"){
+                    aboutSelect.classList.remove("active")
+                    aboutSelect.classList.add("inactive")
+                }
+                loadMenu()
+            }
+            
+            else if(target[0] == "homeBtn"){
+                content.textContent= ""
+                homeSelect.classList.remove("inactive")
+                homeSelect.classList.add("active")
+                if (menuSelect.classList[1] == "active"){
+                    menuSelect.classList.remove("active")
+                    menuSelect.classList.add("inactive")
+                }
+                else if (aboutSelect.classList[1] == "active"){
+                    aboutSelect.classList.remove("active")
+                    aboutSelect.classList.add("inactive")
+                }
+                loadHome()
             }
             
 
-            
+
             /* 
             
             if Menu load Menu.js
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,10 +1,73 @@
 
 import hotdogs from './images/hotdogs.jpg'
 import soup from './images/soup.jpg'
-import salad from '.images/salad.jpg'
+import salad from './images/salad.jpg'
 
 
-function loadMenu() {}
+function loadMenu() {
+    const menuTab = document.createElement('div')
+    menuTab.classList.add("menuTab")
+    content.appendChild(menuTab)
+
+    const welcome = document.createElement('h2')
+    welcome.textContent = "Menu" 
+    menuTab.appendChild(welcome)
+
+    const span1 = document.createElement('span')
+    menuTab.appendChild.span
+
+    const menuItemFirst = document.createElement('div')
+    menuItemFirst.classList.add("menuItemDiv")
+    menuTab.appendChild(menuItemFirst)
+    const hotdogsImg = new Image()
+    hotdogsImg.src = hotdogs
+    menuItemFirst.appendChild(hotdogsImg)
+    const menuItemFirstHeader = document.createElement('h3')
+    menuItemFirstHeader.textContent = "HotDogPlater"
+    menuItemFirst.appendChild(menuItemFirstHeader)
+    const priceItemSmallFirst = document.createElement('div')
+    priceItemSmallFirst.textContent = "6 Dogs --- 6.99"
+    menuItemFirst.appendChild(priceItemSmallFirst)
+    const priceItemLargeFirst = document.createElement('div')
+    priceItemLargeFirst.textContent = "12 Dogs --- 12.99"
+    menuItemFirst.appendChild(priceItemLargeFirst)
+
+
+
+    const menuItemSecond = document.createElement('div')
+    menuItemSecond.classList.add("menuItemDiv")
+    menuTab.appendChild(menuItemSecond)
+    const soupImg = new Image()
+    soupImg.src = soup
+    menuItemSecond.appendChild(soupImg)
+    const menuItemSecondHeader = document.createElement('h3')
+    menuItemSecondHeader.textContent = "Soup"
+    menuItemSecond.appendChild(menuItemSecondHeader)
+    const priceItemSmallSecond = document.createElement('div')
+    priceItemSmallSecond.textContent = "Small --- 4.99"
+    menuItemSecond.appendChild(priceItemSmallSecond)
+    const priceItemLargeSecond = document.createElement('div')
+    priceItemLargeSecond.textContent = "Large --- 7.99"
+    menuItemSecond.appendChild(priceItemLargeSecond)
+
+  
+    const menuItemThird = document.createElement('div')
+    menuItemThird.classList.add("menuItemDiv")
+    menuTab.appendChild(menuItemThird)
+    const saladImg = new Image()
+    saladImg.src = salad
+    menuItemThird.appendChild(saladImg)
+    const menuItemThirdHeader = document.createElement('h3')
+    menuItemThirdHeader.textContent = "Salad"
+    menuItemThird.appendChild(menuItemThirdHeader)
+    const priceItemSmallThird = document.createElement('div')
+    priceItemSmallThird.textContent = "Small --- 5.99"
+    menuItemThird.appendChild(priceItemSmallThird)
+    const priceItemLargeThird = document.createElement('div')
+    priceItemLargeThird.textContent = "Large --- 8.99"
+    menuItemThird.appendChild(priceItemLargeThird)
+
+}
 
 
 export default loadMenu
