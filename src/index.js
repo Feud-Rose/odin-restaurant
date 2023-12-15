c0f197026d92477633f01c1ff01bc3bed00ef81c
Feud-Rose <145285546+Feud-Rose@users.noreply.github.com>
2023-12-15 12:14:57 -0700

    Add Banner placeholder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 console.log("hotdog")
+import './style.css'
+
+
+(function loadSite() {
+    
+   
+    const body = document.querySelector('body')
+    const content = document.querySelector("#content")
+    
+    const banner = document.createElement('div')
+    banner.classList.add('banner')
+    body.before(banner, content)
+   
+    const restaurantName = document.createElement('h1')
+    restaurantName.textContent = "Bobby's Bistro"
+    banner.appendChild(restaurantName) 
+    
+    const navigation = document.createElement('div')
+    navigation.classList.add('navigation')
+    banner.appendChild(navigation)
+
+
+    const home = document.createElement('div')
+    home.textContent = "Home"
+    navigation.appendChild(home)
+
+    const menu = document.createElement('div')
+    menu.textContent = "Menu"
+    navigation.appendChild(menu)
+
+    const about = document.createElement('div')
+    about.textContent = "About"
+    navigation.appendChild(about)
+
+
+
+    const frontTab = document.createElement('div')
+    frontTab.classList.add("frontTab")
+    content.appendChild(frontTab)
+
+
+  
+
+   
+    
+    
+
+})()
+
